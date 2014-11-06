a5ed187f600cb945a95a7bc3f6b633c747f1307d
elfrasco <elfrasco2046@gmail.com>
2014-11-06 17:47:07 -0300

    Parametrizada el serviceContext de los servicios en una única constante.

--- frontend/public/modules/tipo-materiales/services/tipo-materiales.client.service.js
+++ frontend/public/modules/tipo-materiales/services/tipo-materiales.client.service.js
@@ -1,9 +1,12 @@
 'use strict';
 
 //Tipo materiales service used to communicate Tipo materiales REST endpoints
-angular.module('tipo-materiales').factory('TipoMateriales', ['$resource',
-	function($resource) {
-		return $resource('http://localhost:8080/cnea-trazabilidad-archetype/services/tipo-materiales/:tipoMaterialeId', { tipoMaterialeId: '@_id'
+angular.module('tipo-materiales').factory('TipoMateriales', ['$resource', 'Configuration',
+	function($resource, Configuration) {
+
+		var url = Configuration.serviceContext + 'tipo-materiales/:tipoMaterialeId';
+		
+		return $resource(url, { tipoMaterialeId: '@_id'
 		}, {
 			update: {
 				method: 'PUT'

ac41a56db3597ff079634094d8c2091a3b0f27af
elfrasco <adrianparedes82@gmail.com>
2016-01-07 20:27:33 -0300

    Mensajes flotantes de error para el login (por eventos)

---
+++ frontend/public/modules/core/controllers/application.client.controller.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('core').controller('ApplicationController', ['$scope',
+	function($scope) {
+		
+        $scope.messages = {};
+        
+        $scope.$on('showMessage', function(event, message) {
+            $scope.messages.info = message;
+        });
+        
+        $scope.$on('showError', function(event, error) {
+            console.log('HOLAAAAAAAAAAAAAA');
+            $scope.messages.danger = error;
+        });
+
+        $scope.$on('showWarning', function(event, warning) {
+            $scope.messages.warning = warning;
+        });
+
+        $scope.$on('showSuccess', function(event, success) {
+            $scope.messages.success = success;
+        });        
+	}
+]);
--- frontend/public/modules/security/controllers/login.client.controller.js
+++ frontend/public/modules/security/controllers/login.client.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('security').controller('LoginController', ['$scope', 'AuthenticationService', '$location', 'Configuration',
     function ($scope, AuthenticationService, $location, Configuration) {
-
+        
         $scope.init = function() {            
             $scope.credentials = {
                 username: '',
@@ -21,16 +21,7 @@ angular.module('security').controller('LoginController', ['$scope', 'Authenticat
         var loginError = function(response) {
             var error = null;
             if (response) {
-                switch (response.code) {
-                    case 4: //ACCESS_DENIED
-                        error = 'Nombre de usuario o contraseña inválidos.';
-                        break;
-                    case 9:
-                        error = 'Usuario bloqueado, consulte con su administrador.';
-                        break;
-                    default:
-                        error = 'Error Inesperado.';
-                }
+                error = response.message;
             } else {
                 error = 'Error Inesperado.';
             }
